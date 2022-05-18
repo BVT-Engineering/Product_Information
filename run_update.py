b783a7317b129a3dd2ec5ff3edd4c4c6be7b9c78
Luke Doyle <lukeiandoyle@gmail.com>
2022-05-19 10:56:42 +1200

    test deploy for sync action

--- run_update.py
+++ run_update.py
@@ -1,3 +1,4 @@
+from turtle import st
 import pandas as pd
 from pathlib import Path
 import firebase_admin
@@ -6,21 +7,22 @@ from firebase_admin import firestore
 import os
 import json
 import logging
+import pickle
 
 logger = logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
+
     # Use a service account
     creds_json = json.loads(os.environ["creds"])
-    cred = credentials.Certificate(creds_json)
+    cred = credentials.Certificate("fir-auth-c00d2-984b75e2587b.json")
 
     firebase_admin.initialize_app(cred)
     db = firestore.client()
     files = Path("product_information/data").glob("*.csv")  # get all csvs in your dir.
     for file in files:
         tmp = pd.read_csv(file).to_dict(orient="list")
-        # for key, val in tmp.iteritems():
-        #     doc_ref.
+
         doc_ref = db.collection(u"calculationData").document(str(file.stem))
         doc_ref.set(tmp)
