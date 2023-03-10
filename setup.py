631e6cc045e5b112f1b722fefa03c68427da91c7
Luke Doyle <68032934+lukedoyle7@users.noreply.github.com>
2023-03-10 15:58:14 +1300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Product_Information",
-    version="0.9dev",
+    version="0.10dev",
     packages=find_packages(),
     install_requires=["pandas"],
     package_data={"": ["*.csv"]},
