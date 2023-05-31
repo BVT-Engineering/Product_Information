d9417e31800a8e2733c2b836f7fa88b306b32d65
Luke Doyle <68032934+lukedoyle7@users.noreply.github.com>
2023-05-31 14:07:20 +1200

    Update product_data.py

--- product_information/product_data.py
+++ product_information/product_data.py
@@ -3,11 +3,31 @@ from . import data
 import importlib.resources
 
 
+def convert_df_to_python_types(df):
+    converted_df = pd.DataFrame()
+    
+    for column in df.columns:
+        dtype = df[column].dtype
+        if dtype == 'object':
+            converted_df[column] = df[column].astype(str)
+        elif 'datetime' in str(dtype):
+            converted_df[column] = df[column].astype('datetime64[ns]')
+        elif 'int' in str(dtype):
+            converted_df[column] = df[column].astype(int)
+        elif 'float' in str(dtype):
+            converted_df[column] = df[column].astype(float)
+        else:
+            converted_df[column] = df[column]
+    
+    return converted_df
+
 def get_df_from_file_name(filename):
     """Return a dataframe containing data from specified csv filename"""
 
     path = importlib.resources.open_text(data, filename)
-    return pd.read_csv(path)
+    df = pd.read_csv(path) 
+    standard_df = convert_df_to_python_types(df)
+    return standard_df
 
 def autex_frontier_acoustic_fins():
     """Return a dataframe containing Autex frontier acoustic fin data"""
