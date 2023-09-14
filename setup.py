1dd00146df7adbc0b207b9c0bd3b5456660a5e74
nimash1993 <115510557+nimash1993@users.noreply.github.com>
2023-09-15 11:16:18 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Product_Information",
-    version="0.21",
+    version="0.22",
     packages=find_packages(),
     install_requires=["pandas"],
     package_data={"": ["*.csv"]},
